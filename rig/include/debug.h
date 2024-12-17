47b0267bbfc4115ef4e1c21c7137ff60e5dd098d
Julien <sharpshark13@gmail.com>
2024-12-16 23:06:40 -0500

    Improved debug to fix compilation issues.

--- rig/include/debug.h
+++ rig/include/debug.h
@@ -19,14 +19,38 @@ class Debugger
             #endif
         }
 
-        template<typename... T>
-        void Log(T&&... args)
+        template <typename T>
+        void Log(T&& t) 
         {
             #if USE_SERIAL
-                for(auto &&arg: { args... }) {
-                    Serial.print(arg);
-                }
-                Serial.println("");
+                Serial.println(t);
+            #endif
+        }
+
+        template <typename T1, typename T2>
+        void Log(T1&& t1, T2&& t2) 
+        {
+            #if USE_SERIAL
+                Serial.print(t1);
+                Log(t2);
+            #endif
+        }
+
+        template <typename T1, typename T2, typename T3>
+        void Log(T1&& t1, T2&& t2, T3&& t3) 
+        {
+            #if USE_SERIAL
+                Serial.print(t1);
+                Log(t2, t3);
+            #endif
+        }
+
+        template <typename T1, typename T2, typename T3, typename T4>
+        void Log(T1&& t1, T2&& t2, T3&& t3, T4&& t4) 
+        {
+            #if USE_SERIAL
+                Serial.print(t1);
+                Log(t2, t3, t4);
             #endif
         }
 };
