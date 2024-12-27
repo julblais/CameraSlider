f3157f5d685a6f6d5e733354e993c852edae3889
Julien <sharpshark13@gmail.com>
2024-12-27 18:52:00 -0500

    Simplify debug

--- slider/src/debug.h
+++ slider/src/debug.h
@@ -3,6 +3,8 @@
 
 #define USE_SERIAL 1
 
+#include "src/utils/templateUtils.h"
+
 #ifdef USE_SERIAL
     #include <HardwareSerial.h>
 #endif
@@ -19,38 +21,12 @@ class Debugger
             #endif
         }
 
-        template <typename T>
-        void Log(T&& t) 
+        template <typename... TArgs>
+        void Log(TArgs&&... args)
         {
             #if USE_SERIAL
-                Serial.println(t);
-            #endif
-        }
-
-        template <typename T1, typename T2>
-        void Log(T1&& t1, T2&& t2) 
-        {
-            #if USE_SERIAL
-                Serial.print(t1);
-                Log(t2);
-            #endif
-        }
-
-        template <typename T1, typename T2, typename T3>
-        void Log(T1&& t1, T2&& t2, T3&& t3) 
-        {
-            #if USE_SERIAL
-                Serial.print(t1);
-                Log(t2, t3);
-            #endif
-        }
-
-        template <typename T1, typename T2, typename T3, typename T4>
-        void Log(T1&& t1, T2&& t2, T3&& t3, T4&& t4) 
-        {
-            #if USE_SERIAL
-                Serial.print(t1);
-                Log(t2, t3, t4);
+                PassParamPack{(Serial.print(args), 1)...};
+                Serial.println();
             #endif
         }
 };
