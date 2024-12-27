0bd643b6687c0f464e66aa56141623e865801ced
Julien <sharpshark13@gmail.com>
2024-12-27 16:41:37 -0500

    Improve existing commands when moving left and right.

--- slider/src/app/settings.h
+++ slider/src/app/settings.h
@@ -7,12 +7,13 @@ namespace Slider
 {
     struct Settings
     {
-        enum class Speed : char { S_MED = 0, S_SLOW = 1, S_HIGH = 2 };
-        enum class Curve : char { C_LINEAR = 0, C_QUAD = 1, C_EXP = 2 };
+        enum class Speed : char { S_SLOW, S_MED, S_HIGH, COUNT };
+        enum class Curve : char { C_LINEAR, C_QUAD, C_EXP, COUNT };
         
         static constexpr auto SpeedSlow = Speed::S_SLOW;
         static constexpr auto SpeedMedium = Speed::S_MED;
         static constexpr auto SpeedHigh = Speed::S_HIGH;
+
         static constexpr auto CurveLinear = Curve::C_LINEAR;
         static constexpr auto CurveQuadratic = Curve::C_QUAD;
         static constexpr auto CurveExponential = Curve::C_EXP;
