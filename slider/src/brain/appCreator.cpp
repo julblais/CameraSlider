e2699d0b1d2ed79f5a1012ab536c4b3b30d4b2d5
Julien <sharpshark13@gmail.com>
2025-02-12 23:02:31 -0500

    Fixed compilation errors.

--- slider/src/brain/appCreator.cpp
+++ slider/src/brain/appCreator.cpp
@@ -1,8 +1,6 @@
 #include "src/app/appBase.h"
 #include "appConfig.h"
 
-#define TEST_NETWORK true
-
 #ifdef TEST_NETWORK
     #include "src/brain/netApp.h"
  #else
@@ -14,7 +12,7 @@ using namespace Slider;
 template<>
 std::unique_ptr<AppBase> AppCreator<AppConfig>::Create(const AppConfig &config)
 {
-#if TEST_NETWORK
+#ifdef TEST_NETWORK
     #ifdef IS_RECEIVER
         return std::unique_ptr<AppBase>(new BrainApp(config));
     #endif
