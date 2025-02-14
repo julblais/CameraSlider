ff1fa8faae8d7f95217e39ab437ee04e435efe49
Julien <sharpshark13@gmail.com>
2025-02-14 17:41:33 -0500

    Cleanup.

--- slider/src/core/time/timer.cpp
+++ slider/src/core/time/timer.cpp
@@ -7,79 +7,20 @@
 #include <functional>
 
 using namespace Core;
-/*
-static auto Timers = std::vector<Timer*>();
 
-void Timer::UpdateS(unsigned long appTimeMs)
-{
-    for (auto timer : Timers)
-        timer->ProcessCallback(appTimeMs);
-}
+TimerObj::TimerObj(TimerComponent::Iterator itr, TimerComponent::Container* t)
+    : m_Timer(t), m_Id(itr)
+{}
 
-Timer::Timer(const char* name) :
-    m_Name(name),
-    m_Delay(0),
-    m_Callback(),
-    m_StartTimeMs(0)
-{
-    Timers.push_back(this);
-}
+TimerObj::TimerObj()
+    : m_Timer(nullptr), m_Id()
+{}
 
-Timer::Timer(
-    const char* name, std::function<void(unsigned long time)> callback, unsigned long delay) :
-    Timer(name)
-{
-    m_Callback = callback;
-    m_Delay = delay;
-}
+TimerComponent::TimerComponent()
+    :m_TimeMs(0), m_Callbacks()
+{}
 
-Timer::~Timer()
-{
-    auto position = std::find(Timers.begin(), Timers.end(), this);
-    if (position != Timers.end())
-        Timers.erase(position);
-}
-
-void Timer::Start()
-{
-    m_StartTimeMs = millis();
-    LogDebug("Timer \"", m_Name, "\" started at: ", m_StartTimeMs);
-}
-
-void Timer::Stop()
-{
-    if (m_StartTimeMs != 0)
-    {
-        LogDebug("Timer \"", m_Name, "\" stopped");
-        m_StartTimeMs = 0;
-    }
-}
-
-void Timer::Trigger()
-{
-    ProcessCallback(ULONG_MAX);
-}
-
-unsigned int Timer::Delta() const
-{
-    return m_StartTimeMs != 0 ? millis() - m_StartTimeMs : 0;
-}
-
-void Timer::ProcessCallback(unsigned long appTimeMs)
-{
-    if (m_StartTimeMs != 0 && m_Callback)
-    {
-        const auto delta = m_StartTimeMs < appTimeMs ? appTimeMs - m_StartTimeMs : 0u;
-        if (delta >= m_Delay)
-        {
-            LogDebug("Timer \"", m_Name, "\" activated at: ", appTimeMs);
-            m_Callback(delta);
-            Stop();
-        }
-    }
-}*/
-
-void Core::TimerComponent::Setup()
+void TimerComponent::Setup()
 {}
 
 void TimerComponent::Update()
@@ -98,18 +39,6 @@ void TimerComponent::Update()
         }
     }
 }
-
-Core::TimerComponent::TimerComponent()
-    :m_TimeMs(0), m_Callbacks()
-{}
-
-TimerObj::TimerObj(TimerComponent::Iterator itr, TimerComponent::Container* t)
-    : m_Timer(t), m_Id(itr)
-{}
-
-Core::TimerObj::TimerObj()
-    : m_Timer(nullptr), m_Id()
-{}
 
 void TimerObj::Start()
 {
--- slider/src/core/time/timer.h
+++ slider/src/core/time/timer.h
@@ -7,31 +7,9 @@
 #include <vector>
 
 namespace Core
-{/*
-    class Timer
-    {
-    public:
-        static void UpdateS(unsigned long appTimeMs);
-
-        Timer(const char* name);
-        Timer(const char* name, std::function<void(unsigned long time)> callback, unsigned long delay);
-        ~Timer();
-
-        void Start();
-        void Stop();
-        void Trigger();
-        unsigned int Delta() const;
-
-    private:
-        void ProcessCallback(unsigned long appTimeMs);
-
-        const char* const m_Name;
-        unsigned long m_Delay;
-        unsigned long m_StartTimeMs;
-        std::function<void(unsigned long)> m_Callback;
-    };
-*/
+{
     using Time = unsigned long;
+    
     struct TimerObj;
 
     class TimerComponent : public Component
