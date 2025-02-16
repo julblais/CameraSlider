bca1d470dd3e448241c27898ba7ae8861092592f
Julien <sharpshark13@gmail.com>
2025-02-16 12:19:29 -0500

    Fix compilation once again.

--- slider/slider.ino
+++ slider/slider.ino
@@ -1,14 +1,14 @@
-#define ESP_32 TRUE;
+#define ESP_32 true
 
-#define C_DpadUpPin             25;
-#define C_DpadDownPin           26;
-#define C_DpadLeftPin           27;
-#define C_DpadRightPin          33;
-#define C_DpadSelectionPin      14;
-#define C_JoystickXPin          26;
-#define C_JoystickYPin          25;
-#define C_JoystickCenterPin     5;
-#define C_StepperStepPin        33;
-#define C_StepperDirectionPin   32;
-#define C_LcdAddress            0x27;
-#define C_ShowMenuDelayMs       2000;
+#define C_DpadUpPin             25
+#define C_DpadDownPin           26
+#define C_DpadLeftPin           27
+#define C_DpadRightPin          33
+#define C_DpadSelectionPin      14
+#define C_JoystickXPin          26
+#define C_JoystickYPin          25
+#define C_JoystickCenterPin     5
+#define C_StepperStepPin        33
+#define C_StepperDirectionPin   32
+#define C_LcdAddress            0x27
+#define C_ShowMenuDelayMs       2000
