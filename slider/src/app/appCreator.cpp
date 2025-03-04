31b17f140e281a0f506fd70c3969a8fe236b5bf2
Julien <sharpshark13@gmail.com>
2025-03-04 09:39:24 -0500

    Fixed compilation issues.

--- slider/src/app/appCreator.cpp
+++ slider/src/app/appCreator.cpp
@@ -1,13 +1,11 @@
 #include "src/core/app/appBase.h"
 #include "appConfig.h"
 
-#ifdef IS_BRAIN
+#if defined(IS_BRAIN)
 #include "src/app/brain/brainApp.h"
-#endif
-#ifdef IS_CONTROLLER
+#elif defined(IS_CONTROLLER)
 #include "src/app/controller/controllerApp.h"
-#endif
-#ifdef IS_SIMULATOR
+#else // IS_SIMULATOR
 #include "src/app/simulator/simulatorApp.h"
 #endif
 
@@ -17,14 +15,12 @@ using namespace Slider;
 template<>
 std::unique_ptr<AppBase> AppCreator<AppConfig>::Create(const AppConfig & config)
 {
-    #ifdef IS_BRAIN
+    #if defined(IS_BRAIN)
     #include "src/app/brain/brainApp.h"
     return std::unique_ptr<AppBase>(new BrainApp(config));
-    #endif
-    #ifdef IS_CONTROLLER
+    #elif defined(IS_CONTROLLER)
     return std::unique_ptr<AppBase>(new ControllerApp(config));
-    #endif
-    #ifdef IS_SIMULATOR
+    #else // IS_SIMULATOR
     #include "src/app/simulator/simulatorApp.h"
     return std::unique_ptr<AppBase>(new SimulatorApp(config));
     #endif
