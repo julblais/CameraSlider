d7b521186b559c7115436e1a8d6a5d5b72c0f31f
Julien <sharpshark13@gmail.com>
2025-03-05 18:06:13 -0500

    Improve animated printable to auto-start.

--- slider/src/core/output/animatedPrintable.cpp
+++ slider/src/core/output/animatedPrintable.cpp
@@ -1,40 +1,34 @@
 #include "animatedPrintable.h"
+#include <utility>
 
 using namespace Core;
 
-AnimatedPrintable::AnimatedPrintable(const char* name, Messages messages)
-    : m_Messages(messages), m_CurrentMessageIndex(0), m_Timer()
-{
-    m_Timer = Timer::Create(name, [this]() {
-        m_CurrentMessageIndex = (m_CurrentMessageIndex + 1) % m_Messages.size();
-    });
-}
+AnimatedPrintable::AnimatedPrintable(const char* name, unsigned int interval, Messages messages)
+    : m_Messages(messages), m_CurrentMessageIndex(0), m_Interval(interval)
+{}
 
 AnimatedPrintable::AnimatedPrintable(AnimatedPrintable&& other)
-    :m_Messages(std::move(other.m_Messages)),
-    m_Timer(std::move(other.m_Timer)),
+    :m_Messages(std::move(other.m_Messages)), m_Interval(other.m_Interval),
     m_CurrentMessageIndex(other.m_CurrentMessageIndex)
 {}
 
 AnimatedPrintable& Core::AnimatedPrintable::operator=(AnimatedPrintable&& other)
 {
     m_Messages = std::move(other.m_Messages);
-    m_Timer = std::move(other.m_Timer);
     m_CurrentMessageIndex = other.m_CurrentMessageIndex;
     return *this;
 }
 
-void Core::AnimatedPrintable::Start(const unsigned int interval) const
+size_t AnimatedPrintable::printTo(Print& p) const
 {
-    m_Timer.Start(interval, true);
-}
-
-void Core::AnimatedPrintable::Stop() const
-{
-    m_Timer.Stop();
-}
-
-size_t Core::AnimatedPrintable::printTo(Print& p) const
-{
+    if (m_LastTimeMs == 0)
+        m_LastTimeMs = GetTimeMs();
+        
+    auto currentTime = GetTimeMs();
+    if (currentTime - m_LastTimeMs > m_Interval)
+    {
+        m_CurrentMessageIndex = (m_CurrentMessageIndex + 1) % m_Messages.size();
+        m_LastTimeMs = currentTime;
+    }
     return p.print(*(m_Messages.begin() + m_CurrentMessageIndex));
 }
