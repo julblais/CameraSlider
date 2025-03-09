0075ef4cfb729e91a18cf93c70b5080597e0524f
Julien <sharpshark13@gmail.com>
2025-03-09 11:40:31 -0400

    Revert back to using a std::uniqueptr in timer.

--- slider/src/core/time/timer.cpp
+++ slider/src/core/time/timer.cpp
@@ -10,22 +10,6 @@ using namespace Core;
 const int TIMER_QUEUE_LENGTH = 20;
 Queue<Timer::UserData*, TIMER_QUEUE_LENGTH> s_Queue { "Timer queue" };
 
-struct Timer::UserData
-{
-public:
-    UserData(const char* name, Callback callback, bool autoDelete);
-    ~UserData();
-    inline void SetHandle(esp_timer_handle_t handle) { m_Handle = handle; }
-    inline esp_timer_handle_t GetHandle() { return m_Handle; }
-    inline bool ShouldAutoDelete() { return m_AutoDelete; }
-    void Invoke();
-private:
-    const char* m_Name;
-    esp_timer_handle_t m_Handle;
-    const Callback m_Callback;
-    const bool m_AutoDelete;
-};
-
 void TimerComponent::Setup() {}
 
 void TimerComponent::Update()
@@ -63,12 +47,6 @@ void Timer::UserData::Invoke()
 Timer::Timer()
     :m_UserData(nullptr)
 {}
-
-Timer::~Timer()
-{
-    if (m_UserData != nullptr)
-        delete(m_UserData);
-}
 
 Timer::Timer(UserData* userData)
     : m_UserData(userData)
@@ -142,98 +120,5 @@ bool Timer::IsRunning() const
         return esp_timer_is_active(m_UserData->GetHandle());
     return false;
 }
-
-#else
-
-#include "src/debug.h"
-
-#include <esp32-hal-timer.h>
-#include <vector>
-#include <algorithm>
-#include <functional>
-
-using namespace Core;
-
-static unsigned int m_IdGenerator;
-
-Timer::Timer()
-    : m_Timer(nullptr), m_Id(0)
-{}
-
-Timer::Timer(const char* name, TimeManager* timer)
-    : m_Timer(timer), m_Id(++m_IdGenerator)
-{}
-
-void Timer::Start(Time delay) const { m_Timer->Start(m_Id, delay); }
-void Timer::Stop() const { m_Timer->Stop(m_Id); }
-void Timer::Remove() const { m_Timer->Remove(m_Id); }
-
-TimerComponent::TimerComponent()
-    :m_TimeMs(0), m_Timers()
-{
-    m_Timers.reserve(10);
-}
-
-Timer TimerComponent::Create(const char* name, const Timer::Callback& callback)
-{
-    LogDebug("Creating timer: ", name);
-    auto timer = Timer(name, this);
-    m_Timers.emplace_back(name, timer.m_Id, callback);
-    return timer;
-}
-
-void TimerComponent::Update()
-{
-    auto currentTime = GetTimeMs();
-    m_TimeMs = currentTime;
-    for (auto& timer : m_Timers)
-    {
-        if (currentTime >= timer.triggerTime)
-        {
-            LogInfo("Timer \"", timer.name, "\" activated at: ", currentTime);
-            if (timer.cb)
-                timer.cb(currentTime);
-            timer.triggerTime = ULONG_MAX;
-        }
-    }
-}
-
-void TimerComponent::Start(Timer::Id id, Time delay, bool periodic)
-{
-    auto itr = Find(id);
-    LogDebug("Starting timer: ", itr->name);
-    itr->triggerTime = GetTimeMs() + delay;
-}
-
-void TimerComponent::Stop(Timer::Id id)
-{
-    auto itr = Find(id);
-    LogDebug("Stopping timer: ", itr->name);
-    itr->triggerTime = ULONG_MAX;
-}
-
-void TimerComponent::Remove(Timer::Id id)
-{
-    auto itr = Find(id);
-    LogDebug("Removing timer: ", itr->name);
-    m_Timers.erase(itr);
-}
-
-std::vector<TimerComponent::TimerData>::iterator TimerComponent::Find(Timer::Id timerId)
-{
-    auto itr = std::find_if(m_Timers.begin(), m_Timers.end(),
-        [timerId](const TimerData& data) { return data.id == timerId; });
-    assert(itr != m_Timers.end());
-    return itr;
-}
-
-Time TimerComponent::GetCurrentTime() const
-{
-    return m_TimeMs;
-}
-
-TimerComponent::TimerData::TimerData(const char* name, Timer::Id id, const Timer::Callback& callback)
-    : name(name), id(id), cb(callback), triggerTime(ULONG_MAX)
-{}
 
 #endif
--- slider/src/core/time/timer.h
+++ slider/src/core/time/timer.h
@@ -6,6 +6,10 @@
 #include "time.h"
 #include "src/core/component/component.h"
 #include <functional>
+#include <memory>
+
+struct esp_timer;
+typedef struct esp_timer* esp_timer_handle_t;
 
 namespace Core
 {
@@ -18,11 +22,25 @@ namespace Core
     class Timer
     {
     public:
-        struct UserData;
         using Callback = std::function<void(void)>;
+        
+        struct UserData
+        {
+        public:
+            UserData(const char* name, Callback callback, bool autoDelete);
+            ~UserData();
+            inline void SetHandle(esp_timer_handle_t handle) { m_Handle = handle; }
+            inline esp_timer_handle_t GetHandle() { return m_Handle; }
+            inline bool ShouldAutoDelete() { return m_AutoDelete; }
+            void Invoke();
+        private:
+            const char* m_Name;
+            esp_timer_handle_t m_Handle;
+            const Callback m_Callback;
+            const bool m_AutoDelete;
+        };
 
         Timer();
-        ~Timer();
         Timer(const Timer& timer) = delete;
         Timer& operator=(const Timer& timer) = delete;
         Timer(Timer&&) = default;
@@ -39,70 +57,9 @@ namespace Core
         Timer(UserData* userData);
         friend class TimerComponent;
 
-        UserData* m_UserData;
+        std::unique_ptr<UserData> m_UserData;
     };
 }
 
-#else
-
-#include "src/core/component/component.h"
-#include <functional>
-#include <vector>
-
-namespace Core
-{
-    using Time = unsigned long;
-
-    class TimerComponent;
-
-    struct Timer
-    {
-    public:
-        using Callback = std::function<void(Time time)>;
-
-        Timer();
-        void Start(Time delay) const;
-        void Stop() const;
-        void Remove() const;
-
-    private:
-        using Id = unsigned int;
-
-        Timer(const char* name, TimerComponent* timer);
-        Timer::Id m_Id;
-        TimerComponent* m_Timer;
-        friend class TimerComponent;
-    };
-
-    class TimerComponent : public Component
-    {
-    public:
-        TimerComponent();
-        Timer Create(const char* name, const Timer::Callback& callback);
-        virtual void Update() override;
-        Time GetCurrentTime() const;
-
-    private:
-        struct TimerData
-        {
-            TimerData(const char* name, Timer::Id id, const Timer::Callback& callback);
-            const char* name;
-            Timer::Id id;
-            Timer::Callback cb;
-            Time triggerTime;
-        };
-
-        void Start(Timer::Id id, Time delay);
-        void Stop(Timer::Id id);
-        void Remove(Timer::Id id);
-        std::vector<TimerData>::iterator Find(Timer::Id id);
-
-        Time m_TimeMs;
-        std::vector<TimerData> m_Timers;
-        friend struct Timer;
-    };
-
-}
 #endif
-
 #endif
