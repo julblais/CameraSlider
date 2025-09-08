b2a19b9127ada27f9fb56ce43d88cec2ad130059
Julien <sharpshark13@gmail.com>
2025-09-07 20:59:00 -0400

    Merge apps.

--- slider/src/commands/addressCommand.cpp
+++ slider/src/commands/addressCommand.cpp
@@ -1,6 +1,5 @@
 #include "addressCommand.h"
 #include "src/app/settings.h"
-#include "src/network/wifiModule.h"
 
 using namespace Core;
 using namespace Slider;
@@ -8,7 +7,6 @@ using namespace Slider;
 void BrainAddressCommand::Print(Display* display) const
 {
     PrintTitle(display, "Adr. mac");
-    display->PrintLine(1, "  ", Net::WifiModule::GetInstance().GetMacAddress());
 }
 
 void ControllerAddressCommand::Print(Display* display) const
