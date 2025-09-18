1cfd072365e1e354ec2f9947572e2031f66f378e
Julien <sharpshark13@gmail.com>
2025-09-18 11:15:19 -0400

    Renamed variable and improve definition.

--- slider/src/core/utils/enumWrapper.h
+++ slider/src/core/utils/enumWrapper.h
@@ -6,40 +6,25 @@
 namespace Core
 {
     template <typename E, IsTypeEnum<E> = true>
-    class EnumWrapper
+    struct EnumWrapper
     {
-    public:
         constexpr EnumWrapper() noexcept
             : m_Value(0) {}
 
         constexpr EnumWrapper(E value) noexcept
             : m_Value(value) {}
 
-        static constexpr E Zero{};
-
         constexpr bool Has(EnumWrapper flag) const noexcept { return HasValue(m_Value, flag.m_Value); }
-        constexpr bool Any() const noexcept { return m_Value != Zero; }
-        constexpr bool None() const noexcept { return m_Value == Zero; }
+        constexpr bool Any() const noexcept { return m_Value != Default; }
+        constexpr bool None() const noexcept { return m_Value == Default; }
 
-        constexpr EnumWrapper operator|(EnumWrapper rhs) const noexcept
-        {
-            return EnumWrapper(m_Value | rhs.m_Value);
-        }
+        constexpr bool operator==(EnumWrapper rhs) const noexcept { return m_Value == rhs.m_Value; }
+        constexpr bool operator!=(EnumWrapper rhs) const noexcept { return m_Value != rhs.m_Value; }
 
-        constexpr EnumWrapper operator&(EnumWrapper rhs) const noexcept
-        {
-            return EnumWrapper(m_Value & rhs.m_Value);
-        }
-
-        constexpr EnumWrapper operator^(EnumWrapper rhs) const noexcept
-        {
-            return EnumWrapper(m_Value ^ rhs.m_Value);
-        }
-
-        constexpr EnumWrapper operator~() const noexcept
-        {
-            return EnumWrapper(~m_Value);
-        }
+        constexpr EnumWrapper operator|(EnumWrapper rhs) const noexcept { return m_Value | rhs.m_Value; }
+        constexpr EnumWrapper operator&(EnumWrapper rhs) const noexcept { return m_Value & rhs.m_Value; }
+        constexpr EnumWrapper operator^(EnumWrapper rhs) const noexcept { return m_Value ^ rhs.m_Value; }
+        constexpr EnumWrapper operator~() const noexcept { return ~m_Value; }
 
         EnumWrapper& operator|=(EnumWrapper rhs) noexcept
         {
@@ -59,17 +44,8 @@ namespace Core
             return *this;
         }
 
-        constexpr bool operator==(EnumWrapper rhs) const noexcept
-        {
-            return m_Value == rhs.m_Value;
-        }
-
-        constexpr bool operator!=(EnumWrapper rhs) const noexcept
-        {
-            return m_Value != rhs.m_Value;
-        }
-
     private:
+        static constexpr E Default{};
         E m_Value;
     };
 }
